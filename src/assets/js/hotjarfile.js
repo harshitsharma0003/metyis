4b5317f533d46eb4af0ce05bf3d91ceee104fc5b
harshitsharma0003 <harshitsharma0003@gmail.com>
2021-06-02 10:48:56 +0530

    Add files via upload

---
+++ src/assets/js/hotjarfile.js
@@ -0,0 +1,10 @@
+function namibia() {
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1994965,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
